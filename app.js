6f7dd6c10afaf0c97a08d0aa4bf2ec180bab852f
Marek Schwarz <marvoqs@gmail.com>
2020-08-17 17:13:11 +0200

    Add session and passport, delete old authentication

--- app.js
+++ app.js
@@ -3,13 +3,23 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session() {
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set("view engine", "ejs");
 
@@ -19,9 +29,6 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-
-// var secret = process.env.SECRET;
-// userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -34,28 +41,7 @@ app.get("/login", function(req, res) {
 });
 
 app.post("/login", function(req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
 
-  User.findOne({ email: username }, function(err, foundUser) {
-    if (err) {
-      console.log(err);
-    } else {
-      if (foundUser) {
-          bcrypt.compare(password, foundUser.password, function(err, result) {
-            if (result === true) {
-              res.render("secrets");
-            } else {
-              console.log("Wrong user password.");
-              res.render("login");
-            }
-          });
-      } else {
-        console.log("User doesn't exist.");
-        res.render("login");
-      }
-    }
-  });
 });
 
 app.get("/register", function(req, res) {
@@ -63,21 +49,6 @@ app.get("/register", function(req, res) {
 });
 
 app.post("/register", function(req, res) {
-
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-    const newUser = new User({
-      email: req.body.username,
-      password: hash
-    });
-
-    newUser.save(function(err) {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("secrets");
-      }
-    });
-  });
 
 });
 
