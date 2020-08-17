9026cafa8abe6ff8b94228ab8888d69f7dc1396e
Marek Schwarz <marvoqs@gmail.com>
2020-08-17 17:49:17 +0200

    Add new authentication

--- app.js
+++ app.js
@@ -12,18 +12,18 @@ const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(session() {
+app.use(session({
   secret: process.env.SECRET,
   resave: false,
   saveUninitialized: false
-});
+}));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.set("view engine", "ejs");
 
-mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
 const userSchema = new mongoose.Schema({
   email: String,
@@ -49,6 +49,25 @@ app.get("/login", function(req, res) {
 
 app.post("/login", function(req, res) {
 
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const user = new User({
+    username: username,
+    password: password
+  });
+
+  req.login(user, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/secrets");
+      });
+    }
+
+  });
+
 });
 
 app.get("/register", function(req, res) {
@@ -57,6 +76,30 @@ app.get("/register", function(req, res) {
 
 app.post("/register", function(req, res) {
 
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.register({username: username}, password, function(err, user) {
+
+    if (err) {
+      console.log(err);
+      console.redirect("/");
+    } else {
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/secrets");
+      });
+    }
+
+  });
+
+});
+
+app.get("/secrets", function(req, res) {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
 });
 
 app.listen(3000, function() {
